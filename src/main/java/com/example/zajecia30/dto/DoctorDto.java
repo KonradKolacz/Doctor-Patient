ca437d99d33df6bbbb3327a8e9b69c5d963662eb
konradkkkkk <124932337+konradkkkkk@users.noreply.github.com>
2023-02-11 11:14:12 +0100

    Update DoctorDto.java

--- src/main/java/com/example/zajecia30/dto/DoctorDto.java
+++ src/main/java/com/example/zajecia30/dto/DoctorDto.java
@@ -15,7 +15,9 @@ import java.util.Set;
 public class DoctorDto extends RepresentationModel<DoctorDto> {
     private Long id;
     private String name;
+//     changes dev
     private String surname;
+//     dev dev
     private String specialty;
     private Set<Visit> visits;
 //    comment for task34
